0e72fc429ae691780eece111b0dd494dec49a184
newway86 <newway86@yahoo.com>
2018-01-06 21:58:37 +0530

    Initial commit

---
+++ src/test/java/com/sakthi/jenkindemo/JenkindemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sakthi.jenkindemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkindemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("Context Loaded...");
+	}
+
+}
